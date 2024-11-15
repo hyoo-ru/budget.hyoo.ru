1a4ec36baba6a833454d7f33d4b7ae0858c71bc3
nin-jin <sairi-na-tenshi@ya.ru>
2024-11-15 13:37:14 +0000

    Deploying to gh-pages from @ hyoo-ru/budget.hyoo.ru@8daa96cc33b2ef0e6600e68a210678e63f5ce451 🚀

--- node.js
+++ node.js
@@ -14654,7 +14654,7 @@ var $;
     var $$;
     (function ($$) {
         $mol_style_define($hyoo_budget_app, {
-            '--mol_theme_hue': `520deg`,
+            '--mol_theme_hue': `530deg`,
             Menu: {
                 flex: {
                     basis: `15rem`,
