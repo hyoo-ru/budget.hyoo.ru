27aefbeed93db04b6070a59ca7800014270be5ad
nin-jin <nin-jin@users.noreply.github.com>
2024-11-17 22:24:47 +0000

    Deploying to gh-pages from @ hyoo-ru/budget.hyoo.ru@4633ea0161dd6f875189c25d6bcfbcb6336797f5 🚀

--- web.js
+++ web.js
@@ -12964,6 +12964,7 @@ var $;
 var $;
 (function ($) {
     $.$hyoo_crus_yard.masters = [
+        'https://crus.85-92-119-30.ip.hyoo.ru/',
         'https://crus.hyoo.ru/',
         'https://i-love-crus.ru/',
         'https://crus.xedth.ru/',
