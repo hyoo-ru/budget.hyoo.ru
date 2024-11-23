7ad528d017262f56a7f2ee3f39e171a799cfc395
nin-jin <nin-jin@users.noreply.github.com>
2024-11-23 12:42:07 +0000

    Deploying to gh-pages from @ hyoo-ru/budget.hyoo.ru@4633ea0161dd6f875189c25d6bcfbcb6336797f5 🚀

--- node.js
+++ node.js
@@ -11346,6 +11346,8 @@ var $;
                 return vary;
             }
             catch (error) {
+                if (error instanceof Promise)
+                    return $mol_fail_hidden(error);
                 this.$.$mol_fail_log(error);
                 return null;
             }
--- web.js
+++ web.js
@@ -11068,6 +11068,8 @@ var $;
                 return vary;
             }
             catch (error) {
+                if (error instanceof Promise)
+                    return $mol_fail_hidden(error);
                 this.$.$mol_fail_log(error);
                 return null;
             }
