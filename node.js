2e56db27af41a717a1936fa6dd9f5cd2f7690851
nin-jin <nin-jin@users.noreply.github.com>
2024-12-02 08:32:15 +0000

    Deploying to gh-pages from @ hyoo-ru/budget.hyoo.ru@3edd6283ce60dd44f2e473b2a7463693b8240c42 🚀

--- node.js
+++ node.js
@@ -10712,9 +10712,9 @@ var $;
                     message: error,
                     hint: 'Send it to developer',
                 });
+            area.bus();
             area.sync_mine();
             area.sync_yard();
-            area.bus();
             return area;
         }
         Data(Node) {
